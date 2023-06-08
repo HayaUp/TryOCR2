3686e0544560754324d9271081ef9fcb59fdddc9
HayaUp <hayaup.git@gmail.com>
2023-06-08 17:07:51 +0900

    ファイルダイアログで画像を選択し、画像のファイルパスと画像を画面に表示できるようにした。

--- TryOCR2/MainWindow.xaml.cs
+++ TryOCR2/MainWindow.xaml.cs
@@ -24,5 +24,29 @@ namespace TryOCR2
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// ファイルダイアログで画像を選択する
+        /// 選択した画像のファイルパスと画像を画面に表示する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ReadImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var file_dialog = new Microsoft.Win32.OpenFileDialog()
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                DefaultExt = ".png",
+                Filter = "PNG (.png)|*.png",
+            };
+
+            if(file_dialog.ShowDialog() == true)
+            {
+                var image_file_path = file_dialog.FileName;
+                ImageFilePathTextBlock.Text = image_file_path;
+
+                TargetImage.Source = BitmapFrame.Create(new Uri(image_file_path));
+            }
+        }
     }
 }
